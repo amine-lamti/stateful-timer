536f2fad5b810d77d9dbd051734358b7927c67ae
amine-lamti <60108149+amine-lamti@users.noreply.github.com>
2020-02-24 10:19:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Timer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      time: 0,
+      etat:false
+    }
+    setInterval(
+      () => { this.state.etat && 
+        this.setState({ time: this.state.time + 1000 })
+      },
+      1000
+    )  
+  }
+
+  start = () =>{
+    this.setState({etat: !this.state.etat})
+      
+  }
+  reset = () =>{
+    this.setState({time:0,etat:false})
+  }
+
+  render() { 
+    let seconds = Math.floor((this.state.time / 1000) % 60)
+    let minutes = Math.floor((this.state.time / (1000 * 60)) % 60)
+    let hours= Math.floor((this.state.time / (1000 * 60 * 60)) % 24)
+ 
+    return (
+      <div className="chrono">
+        {hours.toString().padStart(2,"0") + ":" + minutes.toString().padStart(2,"0") + ":" + seconds.toString().padStart(2,"0") }
+        <div>
+    <button className="myButton" onClick={this.start}>{this.state.etat ?"pause":"start"}</button>
+        <button className="myButton" onClick={this.reset}>reset</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Timer />
     </div>
   );
 }
